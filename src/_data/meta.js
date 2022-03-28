54f983a7c86bf88b59600251d589187b6b543860
Jason Ryan <jason.w.ryan9@gmail.com>
2022-03-28 01:30:37 -0500

    Update the basics.

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,8 +2,11 @@ module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
   url: process.env.URL || "http://localhost:8080",
-  siteName: "Smol 11ty Starter",
+  siteName: "jwryan.org",
   siteDescription:
-    "Extremely minimal Eleventy starter to kickstart a simple multi-page site / a nearly opinionless foundation to continue building on.",
-  authorName: "Stephanie Eckles", // optional
+    "The personal website Jason Ryan.",
+  authorName: "Jason Ryan", // optional
+  email:"jason@jwryan.org",
+  linkedinUrl: "https://www.linkedin.com/in/jason-ryan-758339b3",
+  githubUrl: "https://github.com/jwryan9"
 };
