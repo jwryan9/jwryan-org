71f46cf33e9e85a5b859542bf12f24417731404d
Jason Ryan <25397692+jwryan9@users.noreply.github.com>
2022-04-05 23:52:29 -0500

    Add Memoji image to site index (#18)
    * add image to page
    
    * style memoji image

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css/');
   eleventyConfig.addWatchTarget('./src/css/');
+  eleventyConfig.addPassthroughCopy('./src/img/');
+  eleventyConfig.addWatchTarget('./src/img/');
 
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
