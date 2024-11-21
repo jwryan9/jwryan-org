7f340c3b2a61d4979369d3415077541585150ab6
Jason Ryan <jryan@theknotww.com>
2024-11-21 15:41:15 -0600

    add redirect for wedding website

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/css/');
   eleventyConfig.addPassthroughCopy('./src/img/');
   eleventyConfig.addWatchTarget('./src/img/');
-
+  eleventyConfig.addPassthroughCopy('./src/_redirects');
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   return {
